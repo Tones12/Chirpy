8c67ea76aa8fd491a809858ad5675d9f3201a5c8
Tones12 <tony.travis12@gmail.com>
2026-07-23 19:30:14 +0000

    Added various handlers per assignment

--- main.go
+++ main.go
@@ -3,18 +3,54 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"sync/atomic"
+	"strconv"
 )
 
-func main() {
-	serveMux := http.NewServeMux()
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+	
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})	
+}
 
-	server := http.Server{
-		Handler:	serveMux,
+func(cfg *apiConfig) resetMetrics(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Swap(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	var apiCfg apiConfig
+	
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Handler:	mux,
 		Addr:		":8080",
 	}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request){
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fileserverHitsStatement := "Hits: " + strconv.FormatInt(int64(apiCfg.fileserverHits.Load()), 10)
+		w.Write([]byte(fileserverHitsStatement))
+	})
+	mux.HandleFunc("/reset", apiCfg.resetMetrics)
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request){
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Printf("Error: %s", err)
 	}
+	
 }
